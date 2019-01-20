4e7425437c102088c085a6bc0902ff1b3a5ef083
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2019-01-20 18:48:42 +0530

    use mock tweets; print result to console

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const Twitter = require('twitter');
+const axios = require('axios');
+const mock = require('./mock.json');
 
 const {
     CONSUMER_KEY,
@@ -24,11 +26,37 @@ const params = {
     screen_name: 'realDonaldTrump',
     tweet_mode: 'extended'
 };
-bot.get('statuses/user_timeline', params)
-    .then(tweets => {
-        const tt = tweets.map(t => t.full_text)
-        console.log(tt);
-    })
-    .catch(error => {
-        console.error(error);
+
+async function trumpposite(tweet) {
+    const words = tweet.split(' ');
+    const promises = words.map(word => {
+        return isAlphaNum(word)
+            ? axios.get(`https://api.datamuse.com/words?rel_ant=${word}`)
+            : Promise.resolve({ data: [] });
     });
+    const responses = await Promise.all(promises);
+    const changedWords = responses.map((res, i) => {
+        const list = res.data;
+        return list.length > 0
+            ? list[getRandomInt(0, list.length)].word
+            : words[i];
+    });
+
+    return changedWords.join(' ');
+}
+
+function getRandomInt(min, max) {
+    // source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function isAlphaNum(word) {
+    return /^[a-z0-9]+$/i.test(word);
+}
+
+const caravan = mock[3];
+trumpposite(caravan)
+    .then(console.log)
+    .catch(console.error);
